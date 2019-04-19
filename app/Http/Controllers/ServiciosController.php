3ebdb5922c63cb5a4ad49d574c58660bf88bac5a
javiersgago <illednas@gmail.com>
2019-04-19 13:34:02 +0100

    No mostrar servicios no asignados a ningún empleado

--- app/Http/Controllers/ServiciosController.php
+++ app/Http/Controllers/ServiciosController.php
@@ -11,12 +11,14 @@ class ServiciosController extends Controller
         $trabajos = Trabajo::all();
         $resultado = array();
         foreach($trabajos as $trabajo) {
-            if (! isset($resultado[$trabajo->categoria]))
-                $resultado[$trabajo->categoria] = array();
-            $resultado2 = new \stdClass();
-            $resultado2->nombre = $trabajo->nombre;
-            $resultado2->precio = number_format($trabajo->precio, 2);
-            array_push($resultado[$trabajo->categoria], $resultado2);
+            if ($trabajo->users()->first()) {
+                if (! isset($resultado[$trabajo->categoria]))
+                    $resultado[$trabajo->categoria] = array();
+                $resultado2 = new \stdClass();
+                $resultado2->nombre = $trabajo->nombre;
+                $resultado2->precio = number_format($trabajo->precio, 2);
+                array_push($resultado[$trabajo->categoria], $resultado2);
+            }
         }
         return view("servicios", ["resultado" => $resultado]);
     }
